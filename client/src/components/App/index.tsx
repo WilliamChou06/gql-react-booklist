d0c9a68f5da5a5ce1a65f142224375f93968254e
William Chou <williamchoudev@gmail.com>
2019-05-23 20:43:15 -0400

    Fix environmental variable CRA error

--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -9,7 +9,7 @@ const UserInput = lazy(() => import('../UserInput'));
 // Check if production:
 // React Google Analytics config
 if (process.env.NODE_ENV === 'production') {
-  ReactGA.initialize(process.env.GA_TRACKING_CODE);
+  ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_CODE);
   ReactGA.pageview(window.location.pathname + window.location.search);
 }
 
